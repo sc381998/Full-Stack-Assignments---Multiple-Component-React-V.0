075e8bba3485aca9dc537bbd2f12af5c02f44533
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-22 16:59:38 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">HealthySmile</h1>
+        <p data-ns-test="project-description">
+          It will be a ecommerce website where healthy, organic and ecofriendly
+          products will be available
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
